3ef8620b9440e2578a7303edbbf07f03f411e84d
Aishat Zakirov <lickey8crp@gmail.com>
2022-08-10 12:50:20 +0300

    tested props

--- src/App.js
+++ src/App.js
@@ -4,19 +4,34 @@ import Image from "./components/image";
 import logo from "./img/logo.jpg"
 
 class App extends React.Component  {
-    helpText = "Help text!"
+    constructor(props) {
+        super(props);
+        this.state = {
+            helpText: "Help text",
+            userData: ''
+        }
+
+        this.inputClick = this.inputClick.bind(this)
+    };
+
+    helpText = "Help text!";
     render() {
         return (<div className="name">
             <Header title="Шапка сайта"/>
-            <h1>{this.helpText}</h1>
-            <input placeholder={this.helpText}
+            <h1>{this.state.helpText}</h1>
+            <h2>{this.state.userData}</h2>
+            <input placeholder={this.state.helpText}
+                   onChange={event => this.setState({userData: event.target.value})}
                    onClick={this.inputClick} onMouseEnter={this.mouseOver} />
-            <p>{this.helpText === "Help text!" ? "Yes" : "No"}</p>
+            <p>{this.state.helpText === "Help text!" ? "Yes" : "No"}</p>
             <Image image={logo} />
         </div>)
-    }
+    };
 
-    inputClick() {console.log("Clicked")}
+    inputClick() {
+        this.setState({helpText: "Changed"});
+        console.log("Clicked");
+    }
     mouseOver() {console.log("Mouse over")}
 };
 
